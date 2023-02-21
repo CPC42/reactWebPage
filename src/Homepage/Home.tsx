4acde000c2cb3aa0de03918c9d5a89e69459eb17
CPC42 <100429042@alumnos.uc3m.es>
2023-02-21 10:32:29 +0100

    Updated pagkages + first components

---
+++ src/Homepage/Home.tsx
@@ -0,0 +1,21 @@
+import { FC, useState } from "react";
+
+function Homepage:FC(){
+
+
+    return(
+        <>
+            <div id>
+                <div><h1 >PAGE TILE</h1></div>
+                <div> <button></button> <button></button></div>
+            </div>
+            <body>
+                
+            </body>
+            <footer>
+
+            </footer>
+        </>
+
+    )
+}
